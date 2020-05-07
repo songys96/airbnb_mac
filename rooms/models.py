9ed020a42b40b8c7e2add1140e8d8ace910fdabf
YunSang <songys96@naver.com>
2020-05-08 00:00:18 +0900

    trying to add amenities and facilities

--- rooms/models.py
+++ rooms/models.py
@@ -3,6 +3,23 @@ from django_countries.fields import CountryField
 from core import models as core_models
 from users import models as user_models
 
+class AbstractItem(core_models.TimeStampedModel):
+
+    """ Abstract Item """
+
+    name = models.CharField(max_length=80)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.name
+
+class RoomType(AbstractItem):
+    
+    pass
+
+
 class Room(core_models.TimeStampedModel):
 
     """ Room Model Definition """
@@ -23,3 +40,6 @@ class Room(core_models.TimeStampedModel):
     check_out = models.TimeField()
     instant_book = models.BooleanField(default=False)
     host = models.ForeignKey(user_models.User, on_delete=models.CASCADE)
+    # user model의 입장에서는 변한게 없다. 여기서만 임의로 연결함 read 의 영역
+
+    room_type = models.ManyToManyField(RoomType, blank=True)
--- users/models.py
+++ users/models.py
@@ -47,5 +47,7 @@ class User(AbstractUser):
     currency = models.CharField(choices=CURRENCY_CHOICES, max_length=6, null=True, blank=True)
     superhost = models.BooleanField(default=False)
     
+    def __str__(self):
+        return self.username
 
 
