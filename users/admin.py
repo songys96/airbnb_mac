07d4ae951a0a8abd3e586a0dd0dea2a648d7a58a
YunSang <songys96@naver.com>
2020-05-07 22:36:11 +0900

    add user model and admin details

--- users/admin.py
+++ users/admin.py
@@ -1,11 +1,42 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from . import models
 
+
 @admin.register(models.User)
-class CustomUserAdmin(admin.ModelAdmin):
-    pass
+class CustomUserAdmin(UserAdmin):
     """
-    it creates user panel on admin
-    admin의 register 함수를 models.User를 인자롤 받아 적용시키는 메서드
-    
+    field를 가지고 있는 기본 포멧으로 나오지만
+    fieldset에 User가 가지고 있는 정보를 넣지 않았으므로 아직 만들어지지 않음
     """
+    fieldsets = UserAdmin.fieldsets + (
+        ("profile", {"fields":("avatar", "gender", "bio", 'birthdate', 'language', 'currency', 'superhost')}),
+    )
+    #("FieldName", {"fields":("attr1", "attr2")}), ...
+        
+
+
+
+# 아래 만든 코드는 직접 하나하나 구현하는 것
+
+# @admin.register(models.User)
+# class CustomUserAdmin(admin.ModelAdmin):
+
+#     """ Custom User Admin """
+
+#     """
+#     it creates user panel on admin
+#     admin의 register 함수를 models.User를 인자롤 받아 적용시키는 데코레이터
+#     it is same as
+#     admin.site.register(models.User, CustomUserAdmin)
+#     """
+#     list_display = ('username', 'gender', 'language', 'currency', 'superhost')
+#     #list_display는 ModelAdmin에 내장된 기능이고 튜플안에 들어갈 내용은 users.models에서 가져온것
+#     list_filter = ('language', 'currency', 'superhost')
+#     empty_value_display = '-empty-'
+#     """ 
+#     자세한 것은 admin site
+#     https://docs.djangoproject.com/en/3.0/ref/contrib/admin/ 
+#     참조
+#     """
+# """
--- users/models.py
+++ users/models.py
@@ -3,4 +3,49 @@ from django.db import models
 
 class User(AbstractUser):
 
-    pass
+    """
+    Custom user model 
+    it designs User Info
+    """
+
+    GENDER_MALE = "male"
+    GENDER_FEMALE = "female"
+    GENDER_OTHER = "other"
+
+    GENDER_CHOICES = (
+        (GENDER_MALE, "Male"),
+        (GENDER_FEMALE, "Female"),
+        (GENDER_OTHER, "Other")
+    )
+
+    LANGUAGE_ENG = "english"
+    LANGUAGE_KOR = "korea"
+
+    LANGUAGE_CHOICES = (
+        (LANGUAGE_ENG, "Eng"),
+        (LANGUAGE_KOR, "Kor")
+    )
+
+    CURRENCY_USD = "usd"
+    CURRENCY_KRW = "krw"
+
+    CURRENCY_CHOICES = (
+        (CURRENCY_USD, "USD"),
+        (CURRENCY_KRW, "KRW")
+    )
+
+    """ 
+    choice의 경우 튜플안의 튜플 구조로 (그룹에 들어갈 이름, 보여질 이름)형식을 맞춰주되 
+    위 처럼 각 필요한 변수의 이름을 지정해 줌으로써 추상화에 신경써줌
+    """
+
+    avatar = models.ImageField(null=True, blank=True)
+    gender = models.CharField(choices=GENDER_CHOICES, max_length=10, null=True, blank=True)
+    bio = models.TextField(default="", blank=True)
+    birthdate = models.DateField(null=True)
+    language = models.CharField(choices=LANGUAGE_CHOICES, max_length=10, null=True, blank=True)
+    currency = models.CharField(choices=CURRENCY_CHOICES, max_length=6, null=True, blank=True)
+    superhost = models.BooleanField(default=False)
+    
+
+
