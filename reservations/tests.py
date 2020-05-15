dd0d2011e53e49589051cc00efaceb46960d4060
YunSang <songys96@naver.com>
2020-05-16 03:57:02 +0900

    edit tests.py

--- reservations/tests.py
+++ reservations/tests.py
@@ -1,28 +1,2 @@
 from django.test import TestCase
-
-import random
-from datetime import datetime, timedelta
 # Create your tests here.
-
-class TimeGenerator():
-    def __init__(self):
-        self.time = datetime.now() + timedelta(days=random.randint(-50,50))
-        print("init", self.time)
-
-    def __str__(self):
-        return str(self.time)
-
-    def __call__(self):
-        print(self.time)
-        new_time = self.time + timedelta(days=3)
-        print(new_time)
-        return new_time
-
-    def cache(self):
-        return
-
-time = TimeGenerator()
-
-formated = {'time':lambda x: time.time()}
-for i in range(3):
-    print(formated['time']("a"))
