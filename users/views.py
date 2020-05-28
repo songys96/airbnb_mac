317379f347683c7f88a3b6fd79f934a6e2abe765
YunSang <songys96@naver.com>
2020-05-28 19:51:15 +0900

    kakao login and add image from kakao profile

--- users/views.py
+++ users/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render, redirect, reverse
 from django.views.generic import FormView
 from django.views import View
 from django.urls import reverse_lazy
+from django.core.files.base import ContentFile
 from . import forms, models
 # Create your views here.
 
@@ -99,6 +100,7 @@ def login_github_callback(request):
                         user = models.User.objects.get(username=username)
                         if user.login_method != models.User.LOGIN_GITHUB:
                             # 2-1. 다른 방식으로 가입했으면 에러발생
+                            """개인적으로 이부분이 에러가 아닌 상태로 가능하도록 했으면 좋겠음.!"""
                             raise GithubException()
                     except models.User.DoesNotExist:
                         # 3. 등록정보가 없으면 새롭게 만들기
@@ -117,15 +119,65 @@ def login_github_callback(request):
     except GithubException:
         return redirect(reverse("users:login"))
 
-def login_kakao(request):
+class KakaoException(Exception):
     pass
+
+def login_kakao(request):
+    client_id = os.environ.get("KAKAO_ID")
+    redirect_uri = "http://localhost:8000/users/login/kakao/callback"
+    return redirect(f"https://kauth.kakao.com/oauth/authorize?client_id={client_id}&redirect_uri={redirect_uri}&response_type=code")
 
 def login_kakao_callback(request):
-    pass
+    try:
+        code = request.GET.get("code")
+        client_id = os.environ.get("KAKAO_ID")
+        redirect_uri = "http://localhost:8000/users/login/kakao/callback"
+        token_request = requests.post(f"https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id={client_id}&redirect_uri={redirect_uri}&code={code}")
+        access_token = token_request.json().get("access_token")
+        profile_request = requests.get(
+            f"https://kapi.kakao.com/v2/user/me", headers={"Authorization":f"Bearer {access_token}"})
+        profile_json = profile_request.json()
+        user_account = profile_json.get("properties", None)
+        user_profile = profile_json.get("kakao_account", None)
+        if user_account is None and user_profile is None:
+            raise KakaoException()
+        nickname = user_account.get("nickname")
+        profile_img = user_account.get("profile_image", None)
+        print("profile_img", profile_img)
+        email = user_profile.get("email")
+        username = email.split("@")[0]
+        print("------",username)
+        try:
+            """ 조군샵의 경우 중복확인을 휴대폰 번호로 검사했음 """
+            user = models.User.objects.get(username=username)
+            if user.login_method != models.User.LOGIN_KAKAO:
+                print("user already exists in other flatform")
+                raise KakaoException()
+        except models.User.DoesNotExist:
 
+            user = models.User.objects.create(
+                username = username,
+                email = email,
+                first_name = nickname,
+                login_method = models.User.LOGIN_KAKAO,
+            )
+            user.set_unusable_password()
+            user.save()
+            if profile_img is not None:
+                # 이미지를 추가할 것인데 
+                # django.core.files.base.contentfile에서 바이너리 상태로 바꾸기
+                img_request = requests.get(profile_img)
+                print(img_request)
+                user.avatar.save(
+                    f"{nickname}_avatar",
+                    ContentFile(img_request.content)
+                )
+        login(request, user)
+        print("done")
+        return redirect(reverse("core:home"))
 
-
-
+    except KakaoException:
+        return redirect(reverse("users:login")) 
 
 
 
